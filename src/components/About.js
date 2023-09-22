9748dc5e0c94e9aca4e2f7055969abbc82cea964
Brahim HM <brahimhm98@gmail.com>
2023-09-22 20:32:38 +0100

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+import Links from "./Links";
+let bio_tag
+function About(props) {
+  if(props.bio){
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <Links github={props.links.github} linkedin={props.links.linkedin}></Links>
+      </div>
+    )
+  }else{
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        {/* add your <Links /> component here */}
+        <Links github={props.links.github} linkedin={props.links.linkedin}></Links>
+      </div>
+    )
+  }
 }
 
 export default About;
